80feb83036637bb1f10da569daabf565b3f7bd1b
[dyad] <realdreamcars78@gmail.com>
2025-10-15 13:21:10 -0700

    [dyad] Ajout de 10 verbes complexes et défectifs - wrote 1 file(s)

--- src/data/verb-data/level3/level3.4.ts
+++ src/data/verb-data/level3/level3.4.ts
@@ -1,17 +1,129 @@
 import { Verb } from '../../verbs';
 
-// This is a placeholder file for a verb.
-// To add a verb, replace 'null' with a verb object.
-// Example:
-//
-// const verb: Verb = {
-//   name: "chanter",
-//   conjugations: {
-//     "présent": { "je": "chante", "tu": "chantes", "il/elle": "chante", "nous": "chantons", "vous": "chantez", "ils/elles": "chantent" },
-//     "futur": { "je": "chanterai", "tu": "chanteras", "il/elle": "chantera", "nous": "chanterons", "vous": "chanterez", "ils/elles": "chanteront" },
-//   },
-// };
-//
-// export default verb;
+const débattre: Verb = {
+  name: "débattre",
+  conjugations: {
+    "indicatif présent": { "je": "débats", "tu": "débats", "il/elle": "débat", "nous": "débattons", "vous": "débattez", "ils/elles": "débattent" },
+    "indicatif imparfait": { "je": "débattais", "tu": "débattais", "il/elle": "débattait", "nous": "débattions", "vous": "débattiez", "ils/elles": "débattaient" },
+    "indicatif passé simple": { "je": "débattis", "tu": "débattis", "il/elle": "débattit", "nous": "débattîmes", "vous": "débattîtes", "ils/elles": "débattirent" },
+    "indicatif futur simple": { "je": "débattrai", "tu": "débattras", "il/elle": "débattra", "nous": "débattrons", "vous": "débattrez", "ils/elles": "débattront" },
+    "indicatif passé composé": { "je": "ai débattu", "tu": "as débattu", "il/elle": "a débattu", "nous": "avons débattu", "vous": "avez débattu", "ils/elles": "ont débattu" },
+    "indicatif plus-que-parfait": { "je": "avais débattu", "tu": "avais débattu", "il/elle": "avait débattu", "nous": "avions débattu", "vous": "aviez débattu", "ils/elles": "avaient débattu" },
+    "indicatif passé antérieur": { "je": "eus débattu", "tu": "eus débattu", "il/elle": "eut débattu", "nous": "eûmes débattu", "vous": "eûtes débattu", "ils/elles": "eurent débattu" },
+    "indicatif futur antérieur": { "je": "aurai débattu", "tu": "auras débattu", "il/elle": "aura débattu", "nous": "aurons débattu", "vous": "aurez débattu", "ils/elles": "auront débattu" },
+    "subjonctif présent": { "je": "que je débatte", "tu": "que tu débattes", "il/elle": "qu'il débatte", "nous": "que nous débattions", "vous": "que vous débattiez", "ils/elles": "qu'ils débattent" },
+    "subjonctif imparfait": { "je": "que je débattisse", "tu": "que tu débattisses", "il/elle": "qu'il débattît", "nous": "que nous débattissions", "vous": "que vous débattissiez", "ils/elles": "qu'ils débattissent" },
+    "subjonctif passé": { "je": "que j'aie débattu", "tu": "que tu aies débattu", "il/elle": "qu'il ait débattu", "nous": "que nous ayons débattu", "vous": "que vous ayez débattu", "ils/elles": "qu'ils aient débattu" },
+    "subjonctif plus-que-parfait": { "je": "que j'eusse débattu", "tu": "que tu eusses débattu", "il/elle": "qu'il eût débattu", "nous": "que nous eussions débattu", "vous": "que vous eussiez débattu", "ils/elles": "qu'ils eussent débattu" },
+    "conditionnel présent": { "je": "débattrais", "tu": "débattrais", "il/elle": "débattrait", "nous": "débattrions", "vous": "débattriez", "ils/elles": "débattraient" },
+    "conditionnel passé première forme": { "je": "aurais débattu", "tu": "aurais débattu", "il/elle": "aurait débattu", "nous": "aurions débattu", "vous": "auriez débattu", "ils/elles": "auraient débattu" },
+    "conditionnel passé deuxième forme": { "je": "eusse débattu", "tu": "eusses débattu", "il/elle": "eût débattu", "nous": "eussions débattu", "vous": "eussiez débattu", "ils/elles": "eussent débattu" },
+    "impératif présent": { "tu": "débats", "nous": "débattons", "vous": "débattez" },
+    "impératif passé": { "tu": "aie débattu", "nous": "ayons débattu", "vous": "ayez débattu" },
+    "infinitif présent": "débattre",
+    "infinitif passé": "avoir débattu",
+    "participe présent": "débattant",
+    "participe passé": "débattu",
+    "gérondif présent": "en débattant",
+    "gérondif passé": "en ayant débattu"
+  }
+};
 
-export default null;
+const promouvoir: Verb = {
+  name: "promouvoir",
+  conjugations: {
+    "indicatif présent": { "je": "promeus", "tu": "promeus", "il/elle": "promeut", "nous": "promouvons", "vous": "promouvez", "ils/elles": "promeuvent" },
+    "indicatif passé simple": { "je": "promus", "tu": "promus", "il/elle": "promut", "nous": "promûmes", "vous": "promûtes", "ils/elles": "promurent" },
+    "indicatif passé composé": { "je": "ai promu", "tu": "as promu", "il/elle": "a promu", "nous": "avons promu", "vous": "avez promu", "ils/elles": "ont promu" },
+    "subjonctif imparfait": { "je": "que je promusse", "tu": "que tu promusses", "il/elle": "qu'il promût", "nous": "que nous promussions", "vous": "que vous promussiez", "ils/elles": "qu'ils promussent" },
+    "participe passé": "promu"
+  }
+};
+
+const prévaloir: Verb = {
+  name: "prévaloir",
+  conjugations: {
+    "indicatif présent": { "je": "prévaux", "tu": "prévaux", "il/elle": "prévaut", "nous": "prévalons", "vous": "prévalez", "ils/elles": "prévalent" },
+    "indicatif futur simple": { "je": "prévoudrai", "tu": "prévoudras", "il/elle": "prévoudra", "nous": "prévoudrons", "vous": "prévoudrez", "ils/elles": "prévoudront" },
+    "subjonctif présent": { "je": "que je prévale", "tu": "que tu prévales", "il/elle": "qu'il prévale", "nous": "que nous prévalions", "vous": "que vous prévaliez", "ils/elles": "qu'ils prévalent" },
+    "conditionnel présent": { "je": "prévoudrais", "tu": "prévoudrais", "il/elle": "prévoudrait", "nous": "prévoudrions", "vous": "prévoudriez", "ils/elles": "prévoudraient" }
+  }
+};
+
+const circonscrire: Verb = {
+  name: "circonscrire",
+  conjugations: {
+    "indicatif présent": { "je": "circonscris", "tu": "circonscris", "il/elle": "circonscrit", "nous": "circonscrivons", "vous": "circonscrivez", "ils/elles": "circonscrivent" },
+    "indicatif passé simple": { "je": "circonscrivis", "tu": "circonscrivis", "il/elle": "circonscrivit", "nous": "circonscrivîmes", "vous": "circonscrivîtes", "ils/elles": "circonscrivirent" },
+    "indicatif passé composé": { "je": "ai circonscrit", "tu": "as circonscrit", "il/elle": "a circonscrit", "nous": "avons circonscrit", "vous": "avez circonscrit", "ils/elles": "ont circonscrit" },
+    "participe passé": "circonscrit"
+  }
+};
+
+const souscrire: Verb = {
+  name: "souscrire",
+  conjugations: {
+    "indicatif présent": { "je": "souscris", "tu": "souscris", "il/elle": "souscrit", "nous": "souscrivons", "vous": "souscrivez", "ils/elles": "souscrivent" },
+    "indicatif passé simple": { "je": "souscrivis", "tu": "souscrivis", "il/elle": "souscrivit", "nous": "souscrivîmes", "vous": "souscrivîtes", "ils/elles": "souscrivirent" },
+    "indicatif passé composé": { "je": "ai souscrit", "tu": "as souscrit", "il/elle": "a souscrit", "nous": "avons souscrit", "vous": "avez souscrit", "ils/elles": "ont souscrit" },
+    "participe passé": "souscrit"
+  }
+};
+
+const extraire: Verb = {
+  name: "extraire",
+  conjugations: {
+    "indicatif présent": { "j'": "extrais", "tu": "extrais", "il/elle": "extrait", "nous": "extrayons", "vous": "extrayez", "ils/elles": "extraient" },
+    "indicatif passé composé": { "j'": "ai extrait", "tu": "as extrait", "il/elle": "a extrait", "nous": "avons extrait", "vous": "avez extrait", "ils/elles": "ont extrait" },
+    "participe passé": "extrait"
+  }
+};
+
+const abstraire: Verb = {
+  name: "abstraire",
+  conjugations: {
+    "indicatif présent": { "j'": "abstrais", "tu": "abstrais", "il/elle": "abstrait", "nous": "abstrayons", "vous": "abstrayez", "ils/elles": "abstraient" },
+    "indicatif passé composé": { "j'": "ai abstrait", "tu": "as abstrait", "il/elle": "a abstrait", "nous": "avons abstrait", "vous": "avez abstrait", "ils/elles": "ont abstrait" },
+    "participe passé": "abstrait"
+  }
+};
+
+const comparaître: Verb = {
+  name: "comparaître",
+  conjugations: {
+    "indicatif présent": { "je": "comparais", "tu": "comparais", "il/elle": "comparaît", "nous": "comparaissons", "vous": "comparaissez", "ils/elles": "comparaissent" },
+    "indicatif passé simple": { "je": "comparus", "tu": "comparus", "il/elle": "comparut", "nous": "comparûmes", "vous": "comparûtes", "ils/elles": "comparurent" },
+    "indicatif passé composé": { "je": "ai comparu", "tu": "as comparu", "il/elle": "a comparu", "nous": "avons comparu", "vous": "avez comparu", "ils/elles": "ont comparu" },
+    "participe passé": "comparu"
+  }
+};
+
+const accroire: Verb = {
+  name: "accroire",
+  conjugations: {
+    "indicatif présent": { "j'": "accrois", "tu": "accrois", "il/elle": "accroit" },
+    "infinitif présent": "accroire"
+  }
+};
+
+const seMéprendre: Verb = {
+  name: "se méprendre",
+  conjugations: {
+    "indicatif présent": { "je": "me méprends", "tu": "te méprends", "il/elle": "se méprend", "nous": "nous méprenons", "vous": "vous méprenez", "ils/elles": "se méprennent" },
+    "indicatif passé composé": { "je": "me suis mépris(e)", "tu": "t'es mépris(e)", "il/elle": "s'est mépris(e)", "nous": "nous sommes mépris(es)", "vous": "vous êtes mépris(es)", "ils/elles": "se sont mépris(es)" },
+    "participe passé": "mépris"
+  }
+};
+
+export default {
+  débattre,
+  promouvoir,
+  prévaloir,
+  circonscrire,
+  souscrire,
+  extraire,
+  abstraire,
+  comparaître,
+  accroire,
+  seMéprendre
+};
