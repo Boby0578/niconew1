a231ce972f7dfd5da9bd2cc10e3d3bc24c25a2fb
[dyad] <realdreamcars78@gmail.com>
2025-10-16 10:07:03 -0700

    [dyad] Ajout de 10 verbes au niveau 3 - wrote 1 file(s)

--- src/data/verb-data/level3/level3.12.ts
+++ src/data/verb-data/level3/level3.12.ts
@@ -1,5 +1,304 @@
 import { Verb } from '../../verbs';
 
-// This is a placeholder file for a verb.
-// To add a verb, replace 'null' with a verb object.
-export default null;
+const querir: Verb = {
+  name: "quérir",
+  conjugations: {
+    "indicatif présent": {},
+    "indicatif imparfait": {},
+    "indicatif passé simple": {},
+    "indicatif futur simple": { "je": "querrai", "tu": "querras", "il/elle": "querra", "nous": "querrons", "vous": "querrez", "ils/elles": "querront" },
+    "indicatif passé composé": { "j'": "ai quis", "tu": "as quis", "il/elle": "a quis", "nous": "avons quis", "vous": "avez quis", "ils/elles": "ont quis" },
+    "indicatif plus-que-parfait": { "j'": "avais quis", "tu": "avais quis", "il/elle": "avait quis", "nous": "avions quis", "vous": "aviez quis", "ils/elles": "avaient quis" },
+    "indicatif passé antérieur": { "j'": "eus quis", "tu": "eus quis", "il/elle": "eut quis", "nous": "eûmes quis", "vous": "eûtes quis", "ils/elles": "eurent quis" },
+    "indicatif futur antérieur": { "j'": "aurai quis", "tu": "auras quis", "il/elle": "aura quis", "nous": "aurons quis", "vous": "aurez quis", "ils/elles": "auront quis" },
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": { "je": "que j'aie quis", "tu": "que tu aies quis", "il/elle": "qu'il ait quis", "nous": "que nous ayons quis", "vous": "que vous ayez quis", "ils/elles": "qu'ils aient quis" },
+    "subjonctif plus-que-parfait": { "je": "que j'eusse quis", "tu": "que tu eusses quis", "il/elle": "qu'il eût quis", "nous": "que nous eussions quis", "vous": "que vous eussiez quis", "ils/elles": "qu'ils eussent quis" },
+    "conditionnel présent": { "je": "querrais", "tu": "querrais", "il/elle": "querrait", "nous": "querrions", "vous": "querriez", "ils/elles": "querraient" },
+    "conditionnel passé première forme": { "j'": "aurais quis", "tu": "aurais quis", "il/elle": "aurait quis", "nous": "aurions quis", "vous": "auriez quis", "ils/elles": "auraient quis" },
+    "conditionnel passé deuxième forme": { "j'": "eusse quis", "tu": "eusses quis", "il/elle": "eût quis", "nous": "eussions quis", "vous": "eussiez quis", "ils/elles": "eussent quis" },
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "quérir",
+    "infinitif passé": "avoir quis",
+    "participe présent": "quérant",
+    "participe passé": "quis",
+    "gérondif présent": "en quérant",
+    "gérondif passé": "en ayant quis"
+  }
+};
+
+const bruire: Verb = {
+  name: "bruire",
+  conjugations: {
+    "indicatif présent": { "il/elle": "bruit" },
+    "indicatif imparfait": { "je": "bruissais", "tu": "bruissais", "il/elle": "bruissait", "nous": "bruissions", "vous": "bruissiez", "ils/elles": "bruissaient" },
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": { "il/elle": "a brui" },
+    "indicatif plus-que-parfait": { "il/elle": "avait brui" },
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": { "il/elle": "qu'il bruisse" },
+    "subjonctif imparfait": {},
+    "subjonctif passé": { "il/elle": "qu'il ait brui" },
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": { "il/elle": "bruirait" },
+    "conditionnel passé première forme": { "il/elle": "aurait brui" },
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "bruire",
+    "infinitif passé": "avoir brui",
+    "participe présent": "bruissant",
+    "participe passé": "brui",
+    "gérondif présent": "en bruissant",
+    "gérondif passé": "en ayant brui"
+  }
+};
+
+const ferir: Verb = {
+  name: "férir",
+  conjugations: {
+    "indicatif présent": {},
+    "indicatif imparfait": {},
+    "indicatif passé simple": { "il/elle": "férit" },
+    "indicatif futur simple": {},
+    "indicatif passé composé": {},
+    "indicatif plus-que-parfait": {},
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": {},
+    "subjonctif imparfait": { "il/elle": "qu'il férît" },
+    "subjonctif passé": {},
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": {},
+    "conditionnel passé première forme": {},
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "férir",
+    "infinitif passé": "",
+    "participe présent": "",
+    "participe passé": "féru",
+    "gérondif présent": "",
+    "gérondif passé": ""
+  }
+};
+
+const tistre: Verb = {
+  name: "tistre",
+  conjugations: {
+    "indicatif présent": {},
+    "indicatif imparfait": {},
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": { "j'": "ai tissu", "tu": "as tissu", "il/elle": "a tissu", "nous": "avons tissu", "vous": "avez tissu", "ils/elles": "ont tissu" },
+    "indicatif plus-que-parfait": { "j'": "avais tissu", "tu": "avais tissu", "il/elle": "avait tissu", "nous": "avions tissu", "vous": "aviez tissu", "ils/elles": "avaient tissu" },
+    "indicatif passé antérieur": { "j'": "eus tissu", "tu": "eus tissu", "il/elle": "eut tissu", "nous": "eûmes tissu", "vous": "eûtes tissu", "ils/elles": "eurent tissu" },
+    "indicatif futur antérieur": { "j'": "aurai tissu", "tu": "auras tissu", "il/elle": "aura tissu", "nous": "aurons tissu", "vous": "aurez tissu", "ils/elles": "auront tissu" },
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": { "je": "que j'aie tissu", "tu": "que tu aies tissu", "il/elle": "qu'il ait tissu", "nous": "que nous ayons tissu", "vous": "que vous ayez tissu", "ils/elles": "qu'ils aient tissu" },
+    "subjonctif plus-que-parfait": { "je": "que j'eusse tissu", "tu": "que tu eusses tissu", "il/elle": "qu'il eût tissu", "nous": "que nous eussions tissu", "vous": "que vous eussiez tissu", "ils/elles": "qu'ils eussent tissu" },
+    "conditionnel présent": {},
+    "conditionnel passé première forme": { "j'": "aurais tissu", "tu": "aurais tissu", "il/elle": "aurait tissu", "nous": "aurions tissu", "vous": "auriez tissu", "ils/elles": "auraient tissu" },
+    "conditionnel passé deuxième forme": { "j'": "eusse tissu", "tu": "eusses tissu", "il/elle": "eût tissu", "nous": "eussions tissu", "vous": "eussiez tissu", "ils/elles": "eussent tissu" },
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "tistre",
+    "infinitif passé": "avoir tissu",
+    "participe présent": "",
+    "participe passé": "tissu",
+    "gérondif présent": "",
+    "gérondif passé": "en ayant tissu"
+  }
+};
+
+const ardre: Verb = {
+  name: "ardre",
+  conjugations: {
+    "indicatif présent": { "il/elle": "ard" },
+    "indicatif imparfait": { "il/elle": "ardait" },
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": {},
+    "indicatif plus-que-parfait": {},
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": {},
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": {},
+    "conditionnel passé première forme": {},
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "ardre",
+    "infinitif passé": "",
+    "participe présent": "ardant",
+    "participe passé": "ars",
+    "gérondif présent": "en ardant",
+    "gérondif passé": ""
+  }
+};
+
+const chaloir: Verb = {
+  name: "chaloir",
+  conjugations: {
+    "indicatif présent": { "il/elle": "chaut" },
+    "indicatif imparfait": {},
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": {},
+    "indicatif plus-que-parfait": {},
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": {},
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": {},
+    "conditionnel passé première forme": {},
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "chaloir",
+    "infinitif passé": "",
+    "participe présent": "",
+    "participe passé": "",
+    "gérondif présent": "",
+    "gérondif passé": ""
+  }
+};
+
+const issir: Verb = {
+  name: "issir",
+  conjugations: {
+    "indicatif présent": {},
+    "indicatif imparfait": {},
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": {},
+    "indicatif plus-que-parfait": {},
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": {},
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": {},
+    "conditionnel passé première forme": {},
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "issir",
+    "infinitif passé": "",
+    "participe présent": "",
+    "participe passé": "issu",
+    "gérondif présent": "",
+    "gérondif passé": ""
+  }
+};
+
+const soudre: Verb = {
+  name: "soudre",
+  conjugations: {
+    "indicatif présent": {},
+    "indicatif imparfait": {},
+    "indicatif passé simple": {},
+    "indicatif futur simple": {},
+    "indicatif passé composé": {},
+    "indicatif plus-que-parfait": {},
+    "indicatif passé antérieur": {},
+    "indicatif futur antérieur": {},
+    "subjonctif présent": {},
+    "subjonctif imparfait": {},
+    "subjonctif passé": {},
+    "subjonctif plus-que-parfait": {},
+    "conditionnel présent": {},
+    "conditionnel passé première forme": {},
+    "conditionnel passé deuxième forme": {},
+    "impératif présent": {},
+    "impératif passé": {},
+    "infinitif présent": "soudre",
+    "infinitif passé": "",
+    "participe présent": "",
+    "participe passé": "",
+    "gérondif présent": "",
+    "gérondif passé": ""
+  }
+};
+
+const guerredonner: Verb = {
+  name: "guerredonner",
+  conjugations: {
+    "indicatif présent": { "je": "guerredonne", "tu": "guerredonnes", "il/elle": "guerredonne", "nous": "guerredonnons", "vous": "guerredonnez", "ils/elles": "guerredonnent" },
+    "indicatif imparfait": { "je": "guerredonnais", "tu": "guerredonnais", "il/elle": "guerredonnait", "nous": "guerredonnions", "vous": "guerredonniez", "ils/elles": "guerredonnaient" },
+    "indicatif passé simple": { "je": "guerredonnai", "tu": "guerredonnas", "il/elle": "guerredonna", "nous": "guerredonnâmes", "vous": "guerredonnâtes", "ils/elles": "guerredonnèrent" },
+    "indicatif futur simple": { "je": "guerredonnerai", "tu": "guerredonneras", "il/elle": "guerredonnera", "nous": "guerredonnerons", "vous": "guerredonnerez", "ils/elles": "guerredonneront" },
+    "indicatif passé composé": { "j'": "ai guerredonné", "tu": "as guerredonné", "il/elle": "a guerredonné", "nous": "avons guerredonné", "vous": "avez guerredonné", "ils/elles": "ont guerredonné" },
+    "indicatif plus-que-parfait": { "j'": "avais guerredonné", "tu": "avais guerredonné", "il/elle": "avait guerredonné", "nous": "avions guerredonné", "vous": "aviez guerredonné", "ils/elles": "avaient guerredonné" },
+    "indicatif passé antérieur": { "j'": "eus guerredonné", "tu": "eus guerredonné", "il/elle": "eut guerredonné", "nous": "eûmes guerredonné", "vous": "eûtes guerredonné", "ils/elles": "eurent guerredonné" },
+    "indicatif futur antérieur": { "j'": "aurai guerredonné", "tu": "auras guerredonné", "il/elle": "aura guerredonné", "nous": "aurons guerredonné", "vous": "aurez guerredonné", "ils/elles": "auront guerredonné" },
+    "subjonctif présent": { "je": "que je guerredonne", "tu": "que tu guerredonnes", "il/elle": "qu'il guerredonne", "nous": "que nous guerredonnions", "vous": "que vous guerredonniez", "ils/elles": "qu'ils guerredonnent" },
+    "subjonctif imparfait": { "je": "que je guerredonnasse", "tu": "que tu guerredonnasses", "il/elle": "qu'il guerredonnât", "nous": "que nous guerredonnassions", "vous": "que vous guerredonnassiez", "ils/elles": "qu'ils guerredonnassent" },
+    "subjonctif passé": { "je": "que j'aie guerredonné", "tu": "que tu aies guerredonné", "il/elle": "qu'il ait guerredonné", "nous": "que nous ayons guerredonné", "vous": "que vous ayez guerredonné", "ils/elles": "qu'ils aient guerredonné" },
+    "subjonctif plus-que-parfait": { "je": "que j'eusse guerredonné", "tu": "que tu eusses guerredonné", "il/elle": "qu'il eût guerredonné", "nous": "que nous eussions guerredonné", "vous": "que vous eussiez guerredonné", "ils/elles": "qu'ils eussent guerredonné" },
+    "conditionnel présent": { "je": "guerredonnerais", "tu": "guerredonnerais", "il/elle": "guerredonnerait", "nous": "guerredonnerions", "vous": "guerredonneriez", "ils/elles": "guerredonneraient" },
+    "conditionnel passé première forme": { "j'": "aurais guerredonné", "tu": "aurais guerredonné", "il/elle": "aurait guerredonné", "nous": "aurions guerredonné", "vous": "auriez guerredonné", "ils/elles": "auraient guerredonné" },
+    "conditionnel passé deuxième forme": { "j'": "eusse guerredonné", "tu": "eusses guerredonné", "il/elle": "eût guerredonné", "nous": "eussions guerredonné", "vous": "eussiez guerredonné", "ils/elles": "eussent guerredonné" },
+    "impératif présent": { "tu": "guerredonne", "nous": "guerredonnons", "vous": "guerredonnez" },
+    "impératif passé": { "tu": "aie guerredonné", "nous": "ayons guerredonné", "vous": "ayez guerredonné" },
+    "infinitif présent": "guerredonner",
+    "infinitif passé": "avoir guerredonné",
+    "participe présent": "guerredonnant",
+    "participe passé": "guerredonné",
+    "gérondif présent": "en guerredonnant",
+    "gérondif passé": "en ayant guerredonné"
+  }
+};
+
+const forfaire: Verb = {
+  name: "forfaire",
+  conjugations: {
+    "indicatif présent": { "je": "forfais", "tu": "forfais", "il/elle": "forfait", "nous": "forfaisons", "vous": "forfaites", "ils/elles": "forfont" },
+    "indicatif imparfait": { "je": "forfaisais", "tu": "forfaisais", "il/elle": "forfaisait", "nous": "forfaisions", "vous": "forfaisiez", "ils/elles": "forfaisaient" },
+    "indicatif passé simple": { "je": "forfis", "tu": "forfis", "il/elle": "forfit", "nous": "forfîmes", "vous": "forfîtes", "ils/elles": "forfirent" },
+    "indicatif futur simple": { "je": "forferai", "tu": "forferas", "il/elle": "forfera", "nous": "forferons", "vous": "forferez", "ils/elles": "forferont" },
+    "indicatif passé composé": { "j'": "ai forfait", "tu": "as forfait", "il/elle": "a forfait", "nous": "avons forfait", "vous": "avez forfait", "ils/elles": "ont forfait" },
+    "indicatif plus-que-parfait": { "j'": "avais forfait", "tu": "avais forfait", "il/elle": "avait forfait", "nous": "avions forfait", "vous": "aviez forfait", "ils/elles": "avaient forfait" },
+    "indicatif passé antérieur": { "j'": "eus forfait", "tu": "eus forfait", "il/elle": "eut forfait", "nous": "eûmes forfait", "vous": "eûtes forfait", "ils/elles": "eurent forfait" },
+    "indicatif futur antérieur": { "j'": "aurai forfait", "tu": "auras forfait", "il/elle": "aura forfait", "nous": "aurons forfait", "vous": "aurez forfait", "ils/elles": "auront forfait" },
+    "subjonctif présent": { "je": "que je forfasse", "tu": "que tu forfasses", "il/elle": "qu'il forfasse", "nous": "que nous forfassions", "vous": "que vous forfassiez", "ils/elles": "qu'ils forfassent" },
+    "subjonctif imparfait": { "je": "que je forfisse", "tu": "que tu forfisses", "il/elle": "qu'il forfît", "nous": "que nous forfissions", "vous": "que vous forfissiez", "ils/elles": "qu'ils forfissent" },
+    "subjonctif passé": { "je": "que j'aie forfait", "tu": "que tu aies forfait", "il/elle": "qu'il ait forfait", "nous": "que nous ayons forfait", "vous": "que vous ayez forfait", "ils/elles": "qu'ils aient forfait" },
+    "subjonctif plus-que-parfait": { "je": "que j'eusse forfait", "tu": "que tu eusses forfait", "il/elle": "qu'il eût forfait", "nous": "que nous eussions forfait", "vous": "que vous eussiez forfait", "ils/elles": "qu'ils eussent forfait" },
+    "conditionnel présent": { "je": "forferais", "tu": "forferais", "il/elle": "forferait", "nous": "forferions", "vous": "forferiez", "ils/elles": "forferaient" },
+    "conditionnel passé première forme": { "j'": "aurais forfait", "tu": "aurais forfait", "il/elle": "aurait forfait", "nous": "aurions forfait", "vous": "auriez forfait", "ils/elles": "auraient forfait" },
+    "conditionnel passé deuxième forme": { "j'": "eusse forfait", "tu": "eusses forfait", "il/elle": "eût forfait", "nous": "eussions forfait", "vous": "eussiez forfait", "ils/elles": "eussent forfait" },
+    "impératif présent": { "tu": "forfais", "nous": "forfaisons", "vous": "forfaites" },
+    "impératif passé": { "tu": "aie forfait", "nous": "ayons forfait", "vous": "ayez forfait" },
+    "infinitif présent": "forfaire",
+    "infinitif passé": "avoir forfait",
+    "participe présent": "forfaisant",
+    "participe passé": "forfait",
+    "gérondif présent": "en forfaisant",
+    "gérondif passé": "en ayant forfait"
+  }
+};
+
+export default {
+  querir,
+  bruire,
+  ferir,
+  tistre,
+  ardre,
+  chaloir,
+  issir,
+  soudre,
+  guerredonner,
+  forfaire
+};
